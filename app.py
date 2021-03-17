06dd0c7411ac602a9b5e8b6ed6af0c9fb64cc33d
FairaIchsaPA <zelov02@yandex.ru>
2021-03-17 10:52:55 +0000

    second commit

--- app.py
+++ app.py
@@ -1,6 +1,20 @@
-from flask import Flask, render_template, url_for
+from flask import Flask, render_template, url_for, request
+import git
 
 app = Flask(__name__)
+
+
+@app.route('/update_server', methods=['POST'])
+def webhook():
+    if request.method == 'POST':
+        repo = git.Repo('./didactic-octo-guide')
+        origin = repo.remotes.origin
+        repo.create_head('master',
+    origin.refs.master).set_tracking_branch(origin.refs.master).checkout()
+        origin.pull()
+        return '', 200
+    else:
+        return '', 400
 
 
 @app.route('/')
